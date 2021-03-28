f2b21343289a291b9a346a33220c4307d476ec84
Szilard Gyorgy <szilard@gyorgy.net>
2021-03-28 12:47:27 +0300

    add new plugin

---
+++ src/hac/plugins/w1_temp/__init__.py
@@ -0,0 +1,36 @@
+import os
+import click
+import click_config_file
+from hac.components.MQTT import MQTT
+from w1thermsensor import W1ThermSensor
+
+
+defaultConfigFile  = "{}/.config/hac/default.cfg".format(os.getenv('HOME'))
+
+
+@click.group()
+def clickcmd(ctx, debug, verbose):
+    pass
+
+
+@clickcmd.command(name="w1temp:list")
+@click.pass_context
+@click_config_file.configuration_option(config_file_name=defaultConfigFile)
+def w1TempList(ctx):
+    for sensor in W1ThermSensor.get_available_sensors():
+        click.echo("Sensor type: {} id: {}".format(sensor.type, sensor.id))
+
+@clickcmd.command(name="w1temp:read")
+@click.option('--sensor-id',
+              required=True,
+              help="Sensor ID")
+@click.option('--sensor-type',
+              required=True,
+              default="40",
+              type=int,
+              help="Sensor TYPE")
+@click.pass_context
+@click_config_file.configuration_option(config_file_name=defaultConfigFile)
+def w1TempRead(ctx, sensor_id, sensor_type):
+    sensor = W1ThermSensor(sensor_type, sensor_id)
+    click.echo("{}°C".format(sensor.get_temperature()))
