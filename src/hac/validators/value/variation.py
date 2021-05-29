56a3d3280b176eace9766a181700747f83f17f1b
Szilard Gyorgy <Szilard Gyorgy>
2021-05-29 18:20:44 +0300

    small fix

--- src/hac/validators/value/variation.py
+++ src/hac/validators/value/variation.py
@@ -1,9 +1,8 @@
 class variation(object):
     @staticmethod
     def filter_invalid(old, new, tolerance):
-        if not isinstance(old, (float, int)):
-            return new
-
-        if abs(float(old) - new) / new * 100 < tolerance:
-            return new
-        return False
+        #print("Old value: {} new value: {}".format(old, new))
+        if not isinstance(old, (float, int)) and new:
+            if abs(float(old) - new) / new * 100 < tolerance:
+                return new
+        return old
